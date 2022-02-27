e58c10057d0f4cd03a6e9b4a9ada571673f9ef66
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2022-02-27 14:00:21 +0530

    Toylor series with recursion.

---
+++ src/main/java/com/aish/DSA/_01recursion/_05ToylorSeriesWithRecursion.java
@@ -0,0 +1,32 @@
+package com.aish.DSA._01recursion;
+
+/**
+ * @author Aishwarya Jorve
+ */
+
+public class _05ToylorSeriesWithRecursion {
+
+//    Variable declaration
+    static int p=1;
+    static int f=1;
+    static int r=0;
+
+
+    public static void main(String[] args) {
+      int r1=eque(4,10);
+        System.out.println(r1);
+    }
+
+//    ToylorSeries method
+    static int eque(int x,int n){
+        if(n==0) {
+            System.out.println("If run");
+            return 1;
+        }
+
+        r=eque(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+p/f;
+    }
+}
