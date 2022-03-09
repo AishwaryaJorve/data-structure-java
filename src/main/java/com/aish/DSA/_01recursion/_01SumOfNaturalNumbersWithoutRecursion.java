e043972f9716483b3dd53d89a7f563fa9d22d234
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2022-03-09 18:48:47 +0530

    Sum of natural numbers without natural numbers

--- src/main/java/com/aish/DSA/_01recursion/_01SumOfNaturalNumbersWithoutRecursion.java
+++ src/main/java/com/aish/DSA/_01recursion/_01SumOfNaturalNumbersWithoutRecursion.java
@@ -1,13 +1,16 @@
 package com.aish.DSA._01recursion;
-
+/**
+ *  @Author Aishwarya Jorve
+ *  Sum of natural numbers without recursion
+ */
 public class _01SumOfNaturalNumbersWithoutRecursion {
 
-    public static void main(String[] args) {
-        int result = sum(5);
-        System.out.println("result: " + result);
-    }
+         public static void main(String[] args) {
+	        int result = sum(5);
+	        System.out.println("result: " + result);
+         }
 
-    public static int sum(int n) {
-        return n * (n + 1) / 2;
-    }
+         public static int sum(int n) {
+	        return n * (n + 1) / 2;
+         }
 }
