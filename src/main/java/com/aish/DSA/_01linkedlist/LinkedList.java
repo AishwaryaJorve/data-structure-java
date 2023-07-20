d38a9d385ac9b9cad8dabd4cb31bced3b7b59f66
AishwaryaJorve <aishwaryajorve1604@gmail.com>
2023-07-20 14:30:07 +0530

    LinkList creation using java with innerclass

---
+++ src/main/java/com/aish/DSA/_01linkedlist/LinkedList.java
@@ -0,0 +1,46 @@
+package com.aish.DSA._01linkedlist;
+
+public class LinkedList
+{
+
+    static Node head;
+
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList linkList=new LinkedList();
+
+        //Creating and inserting node data
+        linkList.head= new Node(2);
+        Node second=new Node(3);
+        Node third=new Node(4);
+        Node fourth=new Node(5);
+
+        //Connection of node
+        LinkedList.head.next=second;
+        second.next=third;
+        third.next=fourth;
+
+        LinkedList.printnode();    //   2-->3-->4-->5-->null
+    }
+
+    private static void printnode() {
+        Node current=head;
+        while(current!=null){
+            System.out.print(current.data+"-->");
+            current=current.next;
+        }
+        System.out.print("null");
+
+    }
+
+}
